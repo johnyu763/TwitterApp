e5625e8c4bcf19ed597080ff871d2fefa0fe076f
johnyu763 <johny11@uci.edu>
2019-11-01 22:21:32 -0700

    Before deleting

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by John Yu on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by John Yu on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
